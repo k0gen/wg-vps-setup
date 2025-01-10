e327da1b59870dabf787101b193d840a7fc051bc
Mariusz Kogen <k0gen@pm.me>
2025-01-10 23:55:04 +0100

    bring back DNS setup for new clients

--- wireguard-install.sh
+++ wireguard-install.sh
@@ -98,6 +98,50 @@ TUN needs to be enabled before running this installer."
 	fi
 fi
 
+new_client_dns () {
+	echo "Select a DNS server for the client:"
+	echo "   1) Current system resolvers"
+	echo "   2) Google"
+	echo "   3) 1.1.1.1"
+	echo "   4) OpenDNS"
+	echo "   5) Quad9"
+	echo "   6) AdGuard"
+	read -p "DNS server [1]: " dns
+	until [[ -z "$dns" || "$dns" =~ ^[1-6]$ ]]; do
+		echo "$dns: invalid selection."
+		read -p "DNS server [1]: " dns
+	done
+		# DNS
+	case "$dns" in
+		1|"")
+			# Locate the proper resolv.conf
+			# Needed for systems running systemd-resolved
+			if grep '^nameserver' "/etc/resolv.conf" | grep -qv '127.0.0.53' ; then
+				resolv_conf="/etc/resolv.conf"
+			else
+				resolv_conf="/run/systemd/resolve/resolv.conf"
+			fi
+			# Extract nameservers and provide them in the required format
+			dns=$(grep -v '^#\|^;' "$resolv_conf" | grep '^nameserver' | grep -v '127.0.0.53' | grep -oE '[0-9]{1,3}(\.[0-9]{1,3}){3}' | xargs | sed -e 's/ /, /g')
+		;;
+		2)
+			dns="8.8.8.8, 8.8.4.4"
+		;;
+		3)
+			dns="1.1.1.1, 1.0.0.1"
+		;;
+		4)
+			dns="208.67.222.222, 208.67.220.220"
+		;;
+		5)
+			dns="9.9.9.9, 149.112.112.112"
+		;;
+		6)
+			dns="94.140.14.14, 94.140.15.15"
+		;;
+	esac
+}
+
 new_client_setup () {
 	# Given a list of the assigned internal IPv4 addresses, obtain the lowest still
 	# available octet. Important to start looking at 2, because 1 is our gateway.
@@ -495,7 +539,8 @@ else
 				client=$(sed 's/[^0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-]/_/g' <<< "$unsanitized_client" | cut -c-15)
 			done
 			echo
-			new_client_dns
+			new_client_
+			
 			new_client_setup
 			# Append new client configuration to the WireGuard interface
 			wg addconf wg0 <(sed -n "/^# BEGIN_PEER $client/,/^# END_PEER $client/p" /etc/wireguard/wg0.conf)
